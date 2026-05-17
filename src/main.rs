b1ad89d63505f8087d6e65bd339bf9618379cfd7
Anmol Patil <anmol@Anmols-MacBook-Pro-2.local>
2026-05-16 21:22:32 -0700

    Add positive unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use jason::parse_from_str;
+
 fn main() {
-    println!("Hello, world!");
+    let example_json = r#"
+    {
+      "id": 1024,
+      "username": "jdoe_99",
+      "email": "john.doe@example.com",
+      "isActive": true ,
+      "roles": ["Admin", "Editor"],
+      "preferences": {
+        "theme": "dark",
+        "notifications": "enabled"
+      },
+      "loginCount": 42
+    }
+    "#;
+    let result = parse_from_str(example_json);
+    match result {
+        Ok(value) => println!("{:?}", value),
+        Err(e) => println!("Error: {}", e),
+    }
 }
