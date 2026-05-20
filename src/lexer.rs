0b0811fd110f8963f71d53e2c5137e4d97d34c93
Anmol Patil <anmol@Anmols-MacBook-Pro-2.local>
2026-05-19 22:58:18 -0700

    Add lexer error position information

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 use crate::JsonError;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Token {
@@ -17,25 +18,61 @@ pub enum Token {
 
 pub struct Lexer {
     chars: Vec<char>,
-    position: usize
+    position: usize,
+    line: usize,
+    column: usize
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Position {
+    line: usize,
+    column: usize,
 }
 
 #[derive(Debug, PartialEq)]
 pub enum LexerError {
-    UnexpectedCharacter,
-    UnterminatedString,
-    InvalidNumber,
-    UnexpectedLiteral,
-    UnterminatedNumber,
-    InvalidUnicodeEscape,
-    UnexpectedEndOfInput
+    UnexpectedCharacter {
+        ch: char,
+        position: Position
+    },
+    UnterminatedString (Position),
+    InvalidNumber (Position),
+    UnexpectedLiteral (Position),
+    UnterminatedNumber (Position),
+    InvalidUnicodeEscape (Position),
+    UnexpectedEndOfInput (Position)
+}
+
+impl Display for LexerError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LexerError::UnexpectedEndOfInput(Position { line, column }) =>
+                write!(f, "Unexpected end of input at line {line}, column {column}"),
+            LexerError::UnexpectedCharacter{
+                ch,
+                position: Position {line, column}
+            } => write!(f, "Unexpected character '{ch}' at line {line} column {column}"),
+            LexerError::InvalidNumber(Position { line, column }) =>
+                write!(f, "Invalid number at line {line}, column {column}"),
+            LexerError::InvalidUnicodeEscape(Position { line, column}) =>
+                write!(f, "Invalid unicode escape at line {line}, column {column}"),
+            LexerError::UnexpectedLiteral(Position{ line, column}) =>
+                write!(f, "Unexpected literal at line {line}, column {column}"),
+            LexerError::UnterminatedNumber(Position{ line, column}) =>
+                write!(f, "Unterminated number at line {line}, column {column}"),
+            LexerError::UnterminatedString(Position { line, column}) =>
+                write!(f, "Unterminated string at line {line}, column {column}")
+        }
+    }
 }
 
 impl Lexer {
     pub fn new(input: &str) -> Self {
         Lexer{
             chars: input.chars().collect(),
-            position: 0
+            position: 0,
+            line: 1,
+            column: 1
         }
     }
 
@@ -46,6 +83,12 @@ impl Lexer {
     fn next(&mut self) -> Option<char> {
         let ch = self.peek()?;
         self.position += 1;
+        if ch == '\n' {
+            self.line += 1;
+            self.column = 1;
+        } else {
+            self.column += 1;
+        }
         Some(ch)
     }
 
@@ -79,7 +122,13 @@ impl Lexer {
             'f' => {self.read_literal("false")?; Ok(Some(Token::False))},
             'n' => {self.read_literal("null")?; Ok(Some(Token::Null))},
             c if c.is_ascii_digit() || c == '-' => Ok(Some(Token::Number(self.read_number()?))),
-            _ => Err(JsonError::Lexer(LexerError::UnexpectedCharacter)),
+            _ => Err(JsonError::Lexer(LexerError::UnexpectedCharacter{
+                ch,
+                position: Position{
+                line: self.line,
+                column: self.column
+            }
+        })),
         }
     }
 
@@ -96,15 +145,24 @@ impl Lexer {
                         Some('\\') => '\\',
                         Some('"') => '"',
                         Some('u') => self.read_unicode_escape()?,
-                        Some(_other) => return Err(JsonError::Lexer(LexerError::InvalidUnicodeEscape)),
-                        None => return Err(JsonError::Lexer(LexerError::UnexpectedEndOfInput))
+                        Some(_other) => return Err(JsonError::Lexer(LexerError::InvalidUnicodeEscape(Position{
+                            line: self.line,
+                            column: self.column
+                        }))),
+                        None => return Err(JsonError::Lexer(LexerError::UnexpectedEndOfInput(Position{
+                            line: self.line,
+                            column: self.column
+                        })))
                     };
                     string_token.push(escaped);
                 }
                 _ => string_token.push(ch)
             };
         }
-        Err(JsonError::Lexer(LexerError::UnterminatedString))
+        Err(JsonError::Lexer(LexerError::UnterminatedString(Position{
+            line: self.line,
+            column: self.column
+        })))
     }
 
     fn read_unicode_escape(&mut self) -> Result<char, JsonError> {
@@ -112,13 +170,22 @@ impl Lexer {
         for _ in 0..4 {
             match self.next() {
                 Some(c) if c.is_ascii_hexdigit() => hex.push(c),
-                _ => return Err(JsonError::Lexer(LexerError::InvalidUnicodeEscape))
+                _ => return Err(JsonError::Lexer(LexerError::InvalidUnicodeEscape(Position{
+                    line: self.line,
+                    column: self.column
+                })))
             }
         }
         let code_point = u32::from_str_radix(&hex, 16)
-                .map_err(|_| JsonError::Lexer(LexerError::InvalidUnicodeEscape))?;
+                .map_err(|_| JsonError::Lexer(LexerError::InvalidUnicodeEscape(Position{
+                    line: self.line,
+                    column: self.column
+                })))?;
         char::from_u32(code_point)
-            .ok_or(JsonError::Lexer(LexerError::InvalidUnicodeEscape))
+            .ok_or(JsonError::Lexer(LexerError::InvalidUnicodeEscape(Position{
+                line: self.line,
+                column: self.column
+            })))
     }
 
     fn read_number(&mut self) -> Result<f64, JsonError> {
@@ -149,18 +216,27 @@ impl Lexer {
             }
         };
         if seen_dot && !digit_after_dot {
-            return Err(JsonError::Lexer(LexerError::UnterminatedNumber))
+            return Err(JsonError::Lexer(LexerError::UnterminatedNumber(Position{
+                line: self.line,
+                column: self.column
+            })))
         }
         number
             .parse::<f64>()
-            .map_err(|_| JsonError::Lexer(LexerError::InvalidNumber))
+            .map_err(|_| JsonError::Lexer(LexerError::InvalidNumber(Position{
+                line: self.line,
+                column: self.column
+            })))
     }
 
     fn read_literal(&mut self, expected: &str) -> Result<(), JsonError> {
         for expected_char in expected.chars() {
             match self.next() {
                 Some(c) if c == expected_char => {},
-                _ => return Err(JsonError::Lexer(LexerError::UnexpectedLiteral)),
+                _ => return Err(JsonError::Lexer(LexerError::UnexpectedLiteral(Position{
+                    line: self.line,
+                    column: self.column
+                }))),
             }
         }
         Ok(())
