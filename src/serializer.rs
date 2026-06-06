c593072d3cfe8101b862a6a1836cc713ae2abcdb
Anmol Patil <anmollp@outlook.com>
2026-06-06 12:50:48 -0700

    Add pretty JSON serializer (#13)
    Co-authored-by: Anmol Patil <anmol@Anmols-MacBook-Pro-2.local>

--- src/serializer.rs
+++ src/serializer.rs
@@ -6,28 +6,12 @@ pub fn to_json_string(value: &JsonValue) -> String {
     output
 }
 
-fn write_string(s: &str, output: &mut String) {
-    output.push('"');
-    for ch in s.chars() {
-        match ch {
-            '"' => output.push_str("\\\""),
-            '\\' => output.push_str("\\\\"),
-            '\n' => output.push_str("\\n"),
-            '\t' => output.push_str("\\t"),
-            _ => output.push(ch)
-        }
-    }
-    output.push('"');
-}
-
 fn serialize(value: &JsonValue, output: &mut String) {
     match value {
         JsonValue::Null => output.push_str("null"),
-        JsonValue::Bool(b) => {
-            match b {
-                true => output.push_str("true"),
-                false => output.push_str("false")
-            }
+        JsonValue::Bool(b) => match b {
+            true => output.push_str("true"),
+            false => output.push_str("false"),
         },
         JsonValue::Number(n) => output.push_str(format!("{}", n).as_str()),
         JsonValue::String(s) => write_string(s, output),
@@ -42,7 +26,7 @@ fn serialize(value: &JsonValue, output: &mut String) {
                 }
             }
             output.push(']');
-        },
+        }
         JsonValue::Object(map) => {
             output.push('{');
             let mut iter = map.iter();
@@ -60,4 +44,82 @@ fn serialize(value: &JsonValue, output: &mut String) {
             output.push('}');
         }
     }
+}
+
+pub fn to_pretty_string(value: &JsonValue) -> String {
+    let mut output = String::new();
+    pretty_serialize(value, &mut output, 0);
+    output
+}
+
+fn pretty_serialize(value: &JsonValue, output: &mut String, indent: usize) {
+    match value {
+        JsonValue::Null => output.push_str("null"),
+        JsonValue::Number(n) => output.push_str(&n.to_string()),
+        JsonValue::String(s) => write_string(s, output),
+        JsonValue::Bool(b) => match b {
+            true => output.push_str("true"),
+            false => output.push_str("false"),
+        },
+        JsonValue::Array(a) => {
+            output.push('[');
+            if a.is_empty() {
+                output.push(']');
+                return;
+            }
+            let len = a.len();
+            for (i, item) in a.iter().enumerate() {
+                output.push('\n');
+                write_indent(output, indent + 1);
+                pretty_serialize(item, output, indent + 1);
+                if i + 1 != len {
+                    output.push(',');
+                }
+            }
+            output.push('\n');
+            write_indent(output, indent);
+            output.push(']')
+        }
+        JsonValue::Object(o) => {
+            output.push('{');
+            if o.is_empty() {
+                output.push('}');
+                return;
+            }
+            let len = o.len();
+            for (i, (key, value)) in o.iter().enumerate() {
+                output.push('\n');
+                write_indent(output, indent + 1);
+                write_string(key, output);
+                output.push_str(": ");
+                pretty_serialize(value, output, indent + 1);
+                if i + 1 != len {
+                    output.push(',');
+                }
+            }
+            output.push('\n');
+            write_indent(output, indent);
+            output.push('}')
+        }
+    }
+}
+
+fn write_indent(output: &mut String, indent: usize) {
+    for _ in 0..indent {
+        output.push_str("  ")
+    }
+}
+
+fn write_string(s: &str, output: &mut String) {
+    output.push('"');
+    for ch in s.chars() {
+        match ch {
+            '"' => output.push_str("\\\""),
+            '\\' => output.push_str("\\\\"),
+            '\n' => output.push_str("\\n"),
+            '\t' => output.push_str("\\t"),
+            _ => output.push(ch),
+        }
+    }
+    output.push('"');
 }
